424e03066ecb5a4108babc8fe5e1daf10c00306e
wanglei8381 <wanglei8381@163.com>
2016-09-30 14:04:06 +0800

    v0.0.2

--- README.md
+++ README.md
@@ -10,4 +10,32 @@ Vue.component('actionsheet', require('vue-m-actionsheet'));
 //&lt;actionsheet>&lt;/actionsheet>
 
 //通过props传值
+props:
+{
+    list: {//显示的数组,数组中对象格式是{label:'显示的内容',value:'值'}
+        type: Array,
+        required: true
+    },
+    open: {//开启关闭
+        type: Boolean,
+        required: true,
+        default: false,
+        twoWay: true
+    },
+    picker: {//接受选择的结果
+        type: Object,
+        // required: true,
+        twoWay: true
+    },
+    label: {//label的别名
+        type: String,
+        required: false,
+        default: 'label'
+    },
+    value: {//value的别名
+        type: String,
+        required: false,
+        default: 'value'
+    }
+}
 </pre>
--- dist/index.js
+++ dist/index.js
@@ -1,54 +1,61 @@
+'use strict';
+
 require('./style.css');
 module.exports = {
     template: require('./template.html'),
-    data(){
-        return {fade: false};
+    data: function data() {
+        return { fade: false };
     },
+
     props: {
-        list: {//显示的数组,数组中对象格式是{label:'显示的内容',value:'值'}
+        list: { //显示的数组,数组中对象格式是{label:'显示的内容',value:'值'}
             type: Array,
             required: true
         },
-        open: {//开启关闭
+        open: { //开启关闭
             type: Boolean,
             required: true,
             default: false,
             twoWay: true
         },
-        picker: {//接受选择的结果
+        picker: { //接受选择的结果
             type: Object,
             // required: true,
             twoWay: true
         },
-        label: {//label的别名
+        label: { //label的别名
             type: String,
             required: false,
             default: 'label'
         },
-        value: {//value的别名
+        value: { //value的别名
             type: String,
             required: false,
             default: 'value'
         }
     },
     watch: {
-        open(val){
+        open: function open(val) {
+            var _this = this;
+
             if (val) {
-                setTimeout(()=> {
-                    this.fade = true;
+                setTimeout(function () {
+                    _this.fade = true;
                 }, 1);
             }
         }
     },
     methods: {
-        choose(item){
+        choose: function choose(item) {
             this.close();
             this.picker = item;
         },
-        close(){
+        close: function close() {
+            var _this2 = this;
+
             this.fade = false;
-            setTimeout(()=> {
-                this.open = false;
+            setTimeout(function () {
+                _this2.open = false;
             }, 300);
         }
     }
